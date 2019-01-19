636db52463cbf8b9165511827754938b7326d9e0
caesar-c4 <caesar13690@gmail.com>
2019-01-19 10:13:47 -0800

    Initial commit

---
+++ crud_2p1/Migrations/20181209223513_AddMorePropiertiesToBook.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace crud_2p1.Migrations
+{
+    public partial class AddMorePropiertiesToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+        }
+    }
+}
---
+++ crud_2p1/Models/book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud_2p1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Author { get; set; }
+    }
+}
